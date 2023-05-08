92deb19b39cc4a031362fb4965a91643bb22d524
ArseniySysyn <arsen.devops@gmail.com>
2023-05-08 22:30:33 +0300

    working

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,8 +69,7 @@ resource "aws_security_group" "mysql" {
     from_port = 3306
     to_port = 3306
     protocol = "tcp"
-    #security_groups = [aws_security_group.app.id]
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.app.id]
   }
 }
 
